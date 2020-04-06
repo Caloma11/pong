56daa638d80c4d1c4265a8683f45c71616fe4478
gianluca <perronegian@hotmail.com>
2020-04-06 15:28:16 +0700

    added collision detection

--- Ball.lua
+++ Ball.lua
@@ -10,10 +10,28 @@ function Ball:init(x, y, width, height)
     self.dy = math.random(-50, 50) -- Delta Y (velocity)
 end
 
+-- Checks if it collides with a 'box' (paddle)
+
+function Ball:collides(box)
+    if self.x > box.x + box.width or self.x + self.width < box.x then
+        return false
+    end
+
+    if self.y > box.y + box.height or self.y + self.height < box.y then
+        return false
+    end
+
+    return true
+end
+
+-- Applies velocity to position scaled  by dt
+
 function Ball:update(dt)
     self.x = self.x + self.dx * dt
     self.y = self.y + self.dy * dt
 end
+
+-- Places ball in the middle with random x and y velocities
 
 function Ball:reset()
     self.x = VIRTUAL_WIDTH / 2 - 2
@@ -23,9 +41,10 @@ function Ball:reset()
     self.dy = math.random(-50, 50) -- Delta Y (velocity)
 end
 
+-- Sets the 'ball' in the middle
 
 function Ball:render()
-    love.graphics.rectangle('fill', self.x, self.y, 4 , 4) -- Sets the 'ball' in the middle
+    love.graphics.rectangle('fill', self.x, self.y, 4 , 4)
 end
 
 
